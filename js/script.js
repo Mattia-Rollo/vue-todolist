3a97f810d8439158741c86aa825f5015eac3134a
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-28 15:18:59 +0200

    add toDosList array and functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-const app = createApp({
+createApp({
     data() {
         return{
+            hasError: false,
+            task: null,
+            newTask: {
+                text: '',
+                done: false
+            },
             toDosList: [
                 {
                     text: 'fare la cacca',
+                    done: false
+                },
+                {
+                    text: 'mangiare',
+                    done: false
+                },
+                {
+                    text: 'lavarsi',
                     done: false
                 }
             ]
         }
     },
     methods: {
+        
+        addTask(){
+            if(this.newTask.text.length > 3){
+            
+            this.toDosList.push(this.newTask);
+            this.newTask = {
+                text: '',
+                done: false
+            };
+            this.hasError = false;
+
+        }else{
+           this.hasError = true; 
+        }},
+        removeTask(i) {
+            this.toDosList.splice(i,1);
+        }
 
     }
 }).mount('#app');
